b89ec06793e0b9ad50a0434f97e3ade3bb911b75
Michael Nwachukwu <dahformulah@gmail.com>
2024-10-01 20:33:02 +0100

    cleanup and deployment

---
+++ ignition/modules/Swap.ts
@@ -0,0 +1,10 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const SwapModule = buildModule("SwapModule", (m) => {
+
+  const swap = m.contract("Swap");
+
+  return { swap };
+});
+
+export default SwapModule;
